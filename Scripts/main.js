e01b243e5fecc3dc5a172cc691c63491a3683c9b
30211705 <elijah.mickelson@ucalgary.ca>
2026-09-24 00:03:40 -0600

    Added Hole (positioning + attaching Mole), and Mole, which is just a stub for now.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,12 @@
 randomiseTitleIcon()
 
 
-// Wwait until the document is fully loaded to get accurate values for window width/height.
+// Wait until the document is fully loaded to get accurate values for window width/height.
 document.addEventListener("DOMContentLoaded", () =>
 {
     updateSize();
 });
+
+new Hole(0, processTreeRoot);
+new Hole(1, processTreeRoot);
+new Hole(2, processTreeRoot);
