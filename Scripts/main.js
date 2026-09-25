da342cbece9997082cbbdee477fb05a64013eac1
Leo Nguyen <leotuananhnguyen@gmail.com>
2026-09-25 12:10:25 -0600

    Added helper function for the penguin's interaction (which darkens the screen)

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,3 +4,14 @@ randomiseTitleIcon()
 new Hole(0, processTreeRoot);
 new Hole(1, processTreeRoot);
 new Hole(2, processTreeRoot);
+
+
+var gameRootElement = document.getElementsByClassName("game-bound")[0];
+
+var timeVal = 0;
+setInterval(() =>
+{
+    timeVal += 0.06;
+    var darknessValue = 0.5 + Math.sin(timeVal) * 0.5;
+    setDarkness(gameRootElement, darknessValue);
+}, 60);
---
+++ Scripts/penguin.js
@@ -0,0 +1,4 @@
+function setDarkness(element, darknessValue)
+{
+    element.style.setProperty("--game-darkness", darknessValue.toString());
+}
