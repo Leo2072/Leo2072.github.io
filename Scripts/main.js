4b51018914c711ec42ca73590447beee45566d2a
Leo Nguyen <leotuananhnguyen@gmail.com>
2026-09-24 23:06:28 -0600

    Deleted the JS code for automatically resizing the whackbox (game area).

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,6 @@
 randomiseTitleIcon()
 
 
-// Wait until the document is fully loaded to get accurate values for window width/height.
-document.addEventListener("DOMContentLoaded", () =>
-{
-    updateSize();
-});
-
 new Hole(0, processTreeRoot);
 new Hole(1, processTreeRoot);
 new Hole(2, processTreeRoot);
